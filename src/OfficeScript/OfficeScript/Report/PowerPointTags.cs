70b3292e7aeb0503871842c52be1fa66e81f202b
Miramac <miramac@gmx.net>
2015-04-21 17:05:23 +0200

    Rename Tags class to PowerPointTags

--- src/OfficeScript/OfficeScript/Report/PowerPointTags.cs
+++ src/OfficeScript/OfficeScript/Report/PowerPointTags.cs
@@ -7,18 +7,21 @@ using PowerPoint = NetOffice.PowerPointApi;
 
 namespace OfficeScript.Report
 {
-    public class Tags
+    public class PowerPointTags
     {
         private dynamic element;
-        public Tags(PowerPoint.Presentation presentation)
+
+        public PowerPointTags(PowerPoint.Presentation presentation)
         {
             this.element = presentation;
         }
-        public Tags(PowerPoint.Slide slide)
+
+        public PowerPointTags(PowerPoint.Slide slide)
         {
             this.element = slide;
         }
-        public Tags(PowerPoint.Shape shape)
+
+        public PowerPointTags(PowerPoint.Shape shape)
         {
             this.element = shape;
         }
@@ -47,6 +50,11 @@ namespace OfficeScript.Report
                     {
                         this.Delete((string)input);
                         return this.Invoke();
+                    }),
+                getAll = (Func<object, Task<object>>)(
+                    async (input) =>
+                    {
+                        return this.GetAll();
                     })
            };
         }
@@ -55,18 +63,39 @@ namespace OfficeScript.Report
         {
             return this.element.Tags[name];
         }
+
+        public Dictionary<string, string> GetAll()
+        {
+            var tags = new Dictionary<string, string>();
+            for (int i = 1; i <= this.element.Tags.Count; i++)
+            {
+                tags.Add(this.element.Tags.Name(i), this.element.Tags.Value(i));
+            }
+            return tags;
+        }
+
         public void Set(Dictionary<string, object> parameters)
         {
             this.Set((string)parameters["name"], (string)parameters["value"]);
         }
+
         public void Set(string name, object value)
         {
             this.Delete(name); //Used tags need to be deleted befor set new
             this.element.Tags.Add(name, value.ToString());
         }
+
         public void Delete(string name)
         {
             this.element.Tags.Delete(name);
         }
+
+        //public void Clone(PowerPointTags dest)
+        //{
+        //    for (int i = 1; i <= this.element.Tags.Count; i++)
+        //    {
+        //        dest.Set(this.element.Tags.Name(i), this.element.Tags.Value(i));
+        //    }
+        //}
     }
 }
